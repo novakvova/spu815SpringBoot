aad80321527aa1513ac08eb33b927dbf1afc5fc9
Volodymyr Novak <novakvova@gmail.com>
2021-02-06 09:58:24 +0200

    show user in MySQL

---
+++ src/main/java/app/entites/User.java
@@ -0,0 +1,55 @@
+package app.entites;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="tblUsers")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/app/web/HomeController.java
+++ src/main/java/app/web/HomeController.java
@@ -1,13 +1,28 @@
 package app.web;
 
+import app.entites.User;
+import app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
 
 @Controller
 public class HomeController {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
         return "index";
     }
 }
